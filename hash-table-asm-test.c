a986a5cf02a8b51eb6e3286ae841dec526322df4
Shreyas Viswanathan <shrevis2018@gmail.com>
2025-01-03 19:35:25 -0600

    ASM_hash + tests

--- hash-table-asm-test.c
+++ hash-table-asm-test.c
@@ -7,6 +7,7 @@
 
 // functions defined in the assembly implementation
 extern Table * ASM_init(long maxWords);
+extern long ASM_hash(Table * table, char * word);
 
 extern bool ASM_lookup(Table * table, char * word);
 extern long ASM_get(Table * table, char * word);
@@ -18,7 +19,8 @@ extern void ASM_print(Table * table);
 extern void ASM_clear(Table * table);
 
 // function forward declarations
-void test_init();
+void test_hash(Table * table);
+void test_init(long maxWords);
 
 void test_lookup(Table * table);
 void test_insert(Table * table);
@@ -29,10 +31,29 @@ void test_get(Table * table);
 void test_clear(Table * table);
 
 int main(int argc, char **argv) {
-   return 0; 
+   Table * table = init(256);
+   assert(table);
+   assert(table->nBuckets == 257);
+   assert(table->maxWords == 256);
+
+   printf("Running hash tests...\n");
+   test_hash(table);
+   printf("Hash tests passed...\n\n");
+
+   return 0;
 }
 
-void test_init() {
+void test_hash(Table * table) {
+  // test for a valid character
+  char *word = "hello";
+  long cHashVal = hash(table, word);
+  long asmHashVal = ASM_hash(table, word);
+
+  assert(cHashVal == asmHashVal);
+  assert(hash(table, NULL) == ASM_hash(table, NULL));
+}
+
+void test_init(long maxWords) {
 
 }
 
--- hash-table-asm.s
+++ hash-table-asm.s
@@ -1,6 +1,27 @@
 .data
+primeNumbers: 
+    .quad 67, 131, 257, 521, 1031, 2053, 4099, 8209, 16411, 32771, 65537, 131073
+nPrimeNumbers: 
+    .quad 12
 
 .text
+initErr:
+    .string "error initializing the array field"
+
+insertErrNode:
+    .string "error allocating memory for a new node"
+
+insertErrWord:
+    .string "error allocating memory for the word field"
+
+bucketNumber:
+    .string "Bucket %ld\n"
+
+nodeFormatOne:
+    .string "Node(Key=%s, Value=%ld)->"
+
+nodeFormatTwo:
+    .string "Node(Key=%s, Value=%ld)\n"
 
 .globl ASM_init
 ASM_init:        # Table * ASM_init(long maxWords)
@@ -14,6 +35,73 @@ ASM_init:        # Table * ASM_init(long maxWords)
 ASM_hash:        # long ASM_hash(Table * table, char * word);
     pushq %rbp
     movq %rsp, %rbp
+
+    pushq %rbx      # push all callee-saved registers in pairs to ensure 16-byte alignment
+    pushq %rbx
+    pushq %r10
+    pushq %r10
+    pushq %r13
+    pushq %r13
+
+    cmpq $0x0, %rsi   # if (word == NULL)
+    je hash_null_word
+
+    movq %rdi, %rbx  # store table in rbx(ensure it doesn't get overwritten)
+    movq %rsi, %r13  # store word in r13
+
+    movq $1, %rdx   # long hashNum = 1
+
+    movq %rsi, %rdi
+    xorq %rax, %rax
+
+    pushq %rdx      # push rdx 2x to ensure 16-byte alignment
+    pushq %rdx
+
+    call strlen
+
+    popq %rdx
+    popq %rdx
+
+    movq %rax, %rcx   # long len = strlen(word);
+    movq $0, %r8     # long i = 0;
+
+while_hash:
+    cmpq %r8, %rcx   # while (i < len)
+    jle break_while_hash
+
+    movb (%r13), %r9b   # load the current character into a byte register
+    movzbq %r9b, %r9    # extend to full quad word so quad operations can be used
+
+    movq %rdx, %rax
+    imulq $31, %rax   # hashNum = 31 * hashNum
+    addq %r9, %rax    # hashNum = 31 * hashNum + word[i]
+    movq %rax, %rdx   # move the result back into hashNum
+
+    addq $1, %r8     # i++
+    addq $1, %r13    # word++
+    jmp while_hash
+
+break_while_hash:
+    movq %rdx, %rax
+    xorq %rdx, %rdx
+    movq 16(%rbx), %r10
+
+    idivq %r10
+
+    movq %rdx, %rax    # we are interested in the remainder so move this
+    jmp finish_hash
+
+hash_null_word:
+    movq $-1, %rax     # return -1
+    jmp finish_hash
+
+finish_hash:
+    popq %r13
+    popq %r13
+    popq %r10
+    popq %r10
+    popq %rbx
+    popq %rbx
 
     leave
     ret
