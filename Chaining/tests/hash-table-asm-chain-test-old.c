47841ed30eb7b1d7b584f29aab179ab665326fe4
Shreyas Viswanathan <shrevis2018@gmail.com>
2025-02-07 18:21:27 -0600

    create separate testing files for old and optimized implementations

---
+++ Chaining/tests/hash-table-asm-chain-test-old.c
@@ -0,0 +1,320 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+#include "../src/hash-table-c-chain.h"
+
+// For testing the old x86 implementation
+
+// functions defined in the assembly implementation
+extern Table * ASM_init(long maxWords);
+extern long ASM_hash(Table * table, char * word);
+
+extern bool ASM_lookup(Table * table, char * word);
+extern long ASM_get(Table * table, char * word);
+extern bool ASM_insert(Table * table, char * word, long value);
+extern bool ASM_delete(Table * table, char * word);
+extern bool ASM_update(Table * table, char * word, long value);
+
+extern void ASM_print(Table * table);
+extern bool ASM_clear(Table * table);
+
+// function forward declarations
+void test_hash(Table * table);
+void test_init();
+
+void test_lookup(Table * table);
+void test_insert(Table * table);
+void test_update(Table * table);
+void test_delete(Table * table);
+void test_get(Table * table);
+
+void test_clear(Table * table);
+
+int main(int argc, char **argv) {
+   printf("Running table init tests...\n");
+   test_init();
+   printf("Table init tests passed...\n\n");
+
+   printf("Running actual table initialization test...\n");
+   long maxWords = 256;
+   Table * table = ASM_init(maxWords);
+   assert(table);
+   assert(table->maxWords == maxWords);
+   assert(table->nBuckets == maxWords + 1);
+   assert(table->array);
+   for (int i = 0; i < table->nBuckets; i++) {
+      assert(table->array[i] == NULL);
+   }
+   printf("Actual table initialization test passed...\n\n");
+
+   printf("Running table hash tests...\n");
+   test_hash(table);
+   printf("Table hash tests passed...\n\n");
+
+   printf("Running table insert tests...\n");
+   test_insert(table);
+   printf("Table insert tests passed...\n\n");
+
+   printf("Running table lookup tests...\n");
+   test_lookup(table);
+   printf("Table lookup tests passed...\n\n");
+
+   printf("Running table get tests...\n");
+   test_get(table);
+   printf("Table get tests passed...\n\n");
+
+   printf("Running table delete tests...\n");
+   test_delete(table);
+   printf("Table delete tests passed...\n\n");
+
+   printf("Running table update tests...\n");
+   test_update(table);
+   printf("Table update tests passed...\n\n");
+
+   printf("Running table clear test...\n");
+   test_clear(table);
+   printf("Table clear test passed...\n");
+
+   return 0;
+}
+
+void test_init() {
+   long maxWordsValidOne = 56;
+   Table * tableOne = ASM_init(maxWordsValidOne);
+   assert(tableOne);
+   assert(tableOne->maxWords == maxWordsValidOne);
+   assert(tableOne->nBuckets == 67);
+   assert(tableOne->array);
+   for (int i = 0; i < tableOne->nBuckets; i++) {
+     assert(tableOne->array[i] == NULL);
+   }
+
+   long maxWordsValidTwo = 113.5;
+   Table * tableTwo = ASM_init(maxWordsValidTwo);
+   assert(tableTwo);
+   assert(tableTwo->maxWords == 113);
+   assert(tableTwo->nBuckets == 131);
+   assert(tableTwo->array);
+   for (int j = 0; j < tableTwo->nBuckets; j++) {
+      assert(tableTwo->array[j] == NULL);
+   }
+
+   long maxWordsInvalid[3] = {-3, 200000, 0};
+   for (int k = 0; k < 3; k++) {
+      Table * curr = ASM_init(maxWordsInvalid[k]);
+      assert(curr == NULL);
+   }
+}
+
+void test_hash(Table * table) {
+  // test for a valid character
+  char *word = "hello";
+  long cHashVal = hash(table, word);
+  long asmHashVal = ASM_hash(table, word);
+
+  assert(cHashVal == asmHashVal);
+
+  // test for invalid word argument
+  assert(ASM_hash(table, NULL) == -1);
+  assert(ASM_hash(table, "") == -1);
+
+  // test for NULL table
+  assert(ASM_hash(NULL, "something") == -1);
+  assert(ASM_hash(NULL, "") == -1);
+  assert(ASM_hash(NULL, NULL) == -1);
+}
+
+void test_insert(Table * table) {
+  // brand new key-value pair
+  char *keyOne = "Hello";
+  long valOne = 15;
+  bool insertOne = ASM_insert(table, keyOne, valOne);
+  assert(insertOne);
+  assert(table->nWords == 1);
+
+  // same key-value pair
+  bool insertTwo = ASM_insert(table, keyOne, valOne);
+  assert(!insertTwo);
+  assert(table->nWords == 1);
+
+  // same key but different value
+  bool insertThree = ASM_insert(table, keyOne, 500);
+  assert(insertThree);
+  assert(table->nWords == 1);
+
+  char *keys[] = {"apple", "banana", "carrot", "dog", "elephant", "fish", "grape", "house", "igloo", "jazz", "vine", "xray", "nest", "yarn", "boat", "under"};
+  long vals[16] = {42, 17, 98765, 1234, 555, 999999, 12, 8765, 333, 45678, 123, 456, 789, 101112, 131415, 161718};
+  int n = sizeof(vals) / sizeof(long);
+
+  for (int i = 0; i < n; i++) {
+    bool insertCurr = ASM_insert(table, keys[i], vals[i]);
+    assert(insertCurr);
+  }
+
+  assert(table->nWords == 17);
+
+  // null key
+  bool insertInvalidOne = ASM_insert(table, NULL, 15);
+  assert(!insertInvalidOne);
+  assert(table->nWords == 17);
+
+  // negative value
+  bool insertInvalidTwo = ASM_insert(table, "hello", -30);
+  assert(!insertInvalidTwo);
+  assert(table->nWords == 17); 
+
+  // NULL table and empty word tests
+  bool insertInvalidThree = ASM_insert(table, "", 15);
+  bool insertInvalidFour = ASM_insert(table, "", -10);
+  bool insertInvalidFive = ASM_insert(table, NULL, -110);
+  bool insertInvalidSix = ASM_insert(NULL, "something", 42);
+  bool insertInvalidSeven = ASM_insert(NULL, "", 32);
+  bool insertInvalidEight = ASM_insert(NULL, "", -23);
+  bool insertInvalidNine = ASM_insert(NULL, NULL, 94);
+  bool insertInvalidTen = ASM_insert(NULL, NULL, -5);
+
+  assert((!insertInvalidThree) && (!insertInvalidFour) && (!insertInvalidFive) && (!insertInvalidSix) && 
+ (!insertInvalidSeven) && (!insertInvalidEight) && (!insertInvalidNine) && (!insertInvalidTen));
+}
+
+void test_lookup(Table * table) {
+   // existent key
+   bool lookupOne = ASM_lookup(table, "banana");
+   assert(lookupOne);
+
+   // non-existent key
+   bool lookupTwo = ASM_lookup(table, "diablo");
+   assert(!lookupTwo);
+
+   // NULL key
+   bool lookupThree = ASM_lookup(table, NULL);
+   assert(!lookupThree);
+
+   // NULL table and empty word tests
+   bool lookupFour = ASM_lookup(NULL, "banana");
+   bool lookupFive = ASM_lookup(NULL, NULL);
+   bool lookupSix = ASM_lookup(NULL, "");
+   bool lookupSeven = ASM_lookup(table, "");
+
+   assert((!lookupFour) && (!lookupFive) && (!lookupSix) && (!lookupSeven));
+}
+
+void test_get(Table * table) {
+  // valid key
+  long getOne = ASM_get(table, "carrot");
+  assert(getOne == 98765);
+
+  // non-existent valid key
+  long getTwo = ASM_get(table, "blablah");
+  assert(getTwo == -1);
+
+  // invalid key
+  long getThree = ASM_get(table, NULL);
+  assert(getThree == -1);
+
+  // NULL table and empty key tests
+  long getFour = ASM_get(table, "");
+  long getFive = ASM_get(NULL, "carrot");
+  long getSix = ASM_get(NULL, "something");
+  long getSeven = ASM_get(NULL, NULL);
+  long getEight = ASM_get(NULL, "");
+
+  assert(getFour + getFive + getSix + getSeven + getEight == -5);
+}
+
+void test_update(Table * table) {
+  // update value of an existing key
+  bool updateOne = ASM_update(table, "apple", 192021);
+  assert(updateOne);
+  assert(table->nWords == 14);
+
+  // update value of a non-existent key
+  bool updateTwo = ASM_update(table, "Kohli", 81);
+  assert(!updateTwo);
+  assert(table->nWords == 15);
+
+  // invalid values
+  bool updateThree = ASM_update(table, NULL, 67);
+  assert(!updateThree);
+
+  bool updateFour = ASM_update(table, "elephant", -25);
+  assert(!updateFour);
+
+  bool updateCurr = ASM_update(table, "SMG", -112);
+  assert(!updateCurr);
+
+  // test NULL table and empty string
+
+  bool updateFive = ASM_update(table, "", 68);
+  bool updateSix = ASM_update(table, "", -76);
+  bool updateSeven = ASM_update(table, NULL, -43);
+  bool updateEight = ASM_update(NULL, "", 90);
+  bool updateNine = ASM_update(NULL, "", -36);
+  bool updateTen = ASM_update(NULL, "blabla", 11);
+  bool updateEleven = ASM_update(NULL, "ajdhjkfe", 34);
+  bool updateTwelve = ASM_update(NULL, NULL, 1111);
+  bool updateThirteen = ASM_update(NULL, NULL, -765);
+
+  assert((!updateFive) && (!updateSix) && (!updateSeven) && (!updateEight) && (!updateNine) && 
+  (!updateTen) && (!updateEleven) && (!updateTwelve) && (!updateThirteen));
+}
+
+void test_delete(Table * table) {
+  // delete head in bucket with > 1 node
+  char * keyOne = "nest";
+  long idxOne = ASM_hash(table, keyOne);
+  bool deleteOne = ASM_delete(table, keyOne);
+  assert(deleteOne);
+  assert(table->nWords == 16);
+  assert(!strcmp(table->array[idxOne]->word, "yarn"));
+
+  // delete non-head in bucket with > 1 node
+  char *keyTwo = "under";
+  long idxTwo = ASM_hash(table, keyTwo);
+  bool deleteTwo = ASM_delete(table, keyTwo);
+  assert(deleteTwo);
+  assert(table->nWords == 15);
+  assert(!strcmp(table->array[idxTwo]->word, "boat"));
+
+  // delete only node in a bucket
+  char *keyThree = "banana";
+  long idxThree = ASM_hash(table, keyThree);
+  bool deleteThree = ASM_delete(table, keyThree);
+  assert(deleteThree);
+  assert(table->nWords == 14);
+  assert(table->array[idxThree] == NULL);
+
+  // delete non-existent key
+  bool deleteFour = ASM_delete(table, "RPG");
+  assert(!deleteFour);
+  assert(table->nWords == 14);
+
+  // delete NULL key
+  bool deleteFive = ASM_delete(table, NULL);
+  assert(!deleteFive);
+  assert(table->nWords == 14);
+
+  // NULL table and empty word tests
+  bool deleteSix = ASM_delete(table, "");
+  bool deleteSeven = ASM_delete(NULL, "banana");
+  bool deleteEight = ASM_delete(NULL, "");
+  bool deleteNine = ASM_delete(NULL, "Rampage");
+
+  assert((!deleteSix) && (!deleteSeven) && (!deleteEight) && (!deleteNine));
+}
+
+void test_clear(Table * table) {
+   // NULL table
+   bool clearOne = ASM_clear(NULL);
+   assert(!clearOne);
+
+   bool clearTwo = ASM_clear(table);
+   assert(clearTwo);
+   for (int i = 0; i < table->nBuckets; i++) {
+    assert(table->array[i] == NULL);
+   }
+
+   assert(table->nWords == 0);
+}
---
+++ Chaining/tests/hash-table-c-chain-test-old.c
@@ -0,0 +1,286 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "../src/hash-table-c-chain.h"
+
+// For testing the old C implementation
+
+void test_init();
+void test_hash(Table * table);
+
+void test_lookup(Table * table);
+void test_insert(Table * table);
+void test_update(Table * table);
+void test_delete(Table * table);
+void test_get(Table * table);
+
+void test_clear(Table * table);
+
+int main(int argc, char **argv) {
+    printf("Running table initialization tests...\n");
+    test_init();
+    printf("Table initialization tests passed...\n\n");
+
+    // will initialize maxWords to 256 and number of buckets to 257
+    printf("Running actual table initialization test...\n");
+    long maxWords = 256;
+    Table * table = init(maxWords);
+    assert(table);
+    assert(table->maxWords == maxWords);
+    assert(table->nBuckets == maxWords + 1);
+    assert(table->array);
+    printf("Actual table initialization test passed...\n\n");
+
+    printf("Running table hash tests...\n");
+    test_hash(table);
+    printf("Table hash tests passed...\n\n");
+    
+    printf("Running table insertion tests...\n");
+    test_insert(table);
+    printf("Table insertion tests passed...\n\n");
+
+    printf("Running table lookup tests...\n");
+    test_lookup(table);
+    printf("Table lookup tests passed...\n\n");
+    
+    printf("Running table update tests...\n");
+    test_update(table);
+    printf("Table update tests passed...\n\n");
+    
+    printf("Running table get tests...\n");
+    test_get(table);
+    printf("Table get tests passed...\n\n");
+
+    printf("Running table delete tests...\n");
+    test_delete(table);
+    printf("Table delete tests passed...\n\n");
+
+    printf("Running table clear tests...\n");
+    test_clear(table);
+    printf("Table clear tests passed...\n\n");
+
+    return 0;
+}
+
+void test_hash(Table * table) {
+    // invalid values
+    assert(hash(table, NULL) == -1);
+    assert(hash(table, "") == -1);
+    assert(hash(NULL, "something") == -1);
+    assert(hash(NULL, "") == -1);
+    assert(hash(NULL, NULL) == -1);
+
+    // general cases
+    char *words[] = {"hello", "bye", "see", "flag"};
+    long indices[4] = {22, 92, 132, 20};
+
+    for (int i = 0; i < 4; i++) {
+        assert(hash(table, words[i]) == indices[i]);
+    }
+}
+
+void test_init() {
+    long maxWordsValidOne = 56;
+    Table * tableOne = init(maxWordsValidOne);
+    assert(tableOne);
+    assert(tableOne->maxWords == maxWordsValidOne);
+    assert(tableOne->nBuckets == 67);
+    assert(tableOne->array);
+
+    long maxWordsValidTwo = 113.5;
+    Table * tableTwo = init(maxWordsValidTwo);
+    assert(tableTwo);
+    assert(tableTwo->maxWords == 113);
+    assert(tableTwo->nBuckets == 131);
+    assert(tableTwo->array);
+
+    long maxWordsInvalid[3] = {-3, 200000, 0};
+    for (int i = 0; i < 3; i++) {
+        Table * curr = init(maxWordsInvalid[i]);
+        assert(curr == NULL);
+    }
+}
+
+void test_insert(Table * table) {
+    // brand new key-value pair
+    char *keyOne = "Hello";
+    long valOne = 15;
+    bool insertOne = insert(table, keyOne, valOne);
+    assert(insertOne);
+    assert(table->nWords == 1);
+
+    // same key-value pair
+    bool insertTwo = insert(table, keyOne, valOne);
+    assert(!insertTwo);
+    assert(table->nWords == 1);
+
+    // same key but different value
+    long valTwo = 500;
+    bool insertThree = insert(table, keyOne, valTwo);
+    assert(insertThree);
+    assert(table->nWords == 1);
+
+    // bunch of random key-value pairs
+        // random distribution + collision-creating
+    char *keys[] = {"apple", "banana", "carrot", "dog", "elephant", "fish", "grape", "house", "igloo", "jazz", "vine", "xray", "nest", "yarn", "boat", "under"};
+    long vals[16] = {42, 17, 98765, 1234, 555, 999999, 12, 8765, 333, 45678, 123, 456, 789, 101112, 131415, 161718};
+    int n = sizeof(vals) / sizeof(long);
+
+    for (int i = 0; i < n; i++) {
+        bool insertCurr = insert(table, keys[i], vals[i]);
+        assert(insertCurr);
+    }
+
+    assert(table->nWords == 17);
+
+    // key is NULL
+    bool insertInvalidOne = insert(table, NULL, 15);
+    assert(!insertInvalidOne);
+    assert(table->nWords == 17);
+
+    // value is less than 0
+    bool insertInvalidTwo = insert(table, "hello", -57);
+    assert(!insertInvalidTwo);
+    assert(table->nWords == 17);
+
+    // mix of invalid insert arguments
+    bool insertInvalidThree = insert(table, "", 15);
+    bool insertInvalidFour = insert(table, "", -10);
+    bool insertInvalidFive = insert(table, NULL, -110);
+    bool insertInvalidSix = insert(NULL, "something", 42);
+    bool insertInvalidSeven = insert(NULL, "", 32);
+    bool insertInvalidEight = insert(NULL, "", -23);
+    bool insertInvalidNine = insert(NULL, NULL, 94);
+    bool insertInvalidTen = insert(NULL, NULL, -5);
+
+    assert((!insertInvalidThree) && (!insertInvalidFour) && (!insertInvalidFive) && (!insertInvalidSix) && (!insertInvalidSeven) && (!insertInvalidEight) && (!insertInvalidNine) && (!insertInvalidTen));
+}
+
+void test_lookup(Table * table) {
+    // existent key
+    char *existent = "banana";
+    bool lookupOne = lookup(table, existent);
+    assert(lookupOne);
+
+    // non-existent key
+    char *nonExistent = "diablo";
+    bool lookupTwo = lookup(table, nonExistent);
+    assert(!lookupTwo);
+
+    // invalid values
+    bool lookupThree = lookup(table, NULL);
+    bool lookupFour = lookup(table, "");
+    bool lookupFive = lookup(NULL, "banana");
+    bool lookupSix = lookup(NULL, "diablo");
+    bool lookupSeven = lookup(NULL, NULL);
+
+    assert((!lookupThree) && (!lookupFour) && (!lookupFive) && (!lookupSix) && (!lookupSeven));
+}
+
+void test_update(Table * table) {
+    // update the value of an existing key
+    char *existingKey = "under";
+    long newValOne = 192021;
+    bool updateOne = update(table, existingKey, newValOne);
+    assert(updateOne);
+    assert(table->nWords == 17);
+
+    // update the value of a non-existent key
+    char *nonExistentKey = "formula";
+    long newValTwo = 489321;
+    bool updateTwo = update(table, nonExistentKey, newValTwo);
+    assert(!updateTwo);
+    assert(table->nWords == 18);
+
+    // invalid values
+    bool updateThree = update(table, NULL, 45);
+    assert(!updateThree);
+
+    bool updateFour = update(table, "elephant", -100);
+    assert(!updateFour);
+
+    // mix of other invalid update args
+    bool updateFive = update(table, "", 68);
+    bool updateSix = update(table, "", -76);
+    bool updateSeven = update(table, NULL, -43);
+    bool updateEight = update(NULL, "", 90);
+    bool updateNine = update(NULL, "", -36);
+    bool updateTen = update(NULL, "blabla", 11);
+    bool updateEleven = update(NULL, "ajdhjkfe", 34);
+    bool updateTwelve = update(NULL, NULL, 1111);
+    bool updateThirteen = update(NULL, NULL, -765);
+
+    assert((!updateFive) && (!updateSix) && (!updateSeven) && (!updateEight) && (!updateNine) && (!updateTen) && (!updateEleven) && (!updateTwelve) && (!updateThirteen));
+}
+
+void test_get(Table * table) {
+    // valid key
+    long getOne = get(table, "carrot");
+    assert(getOne == 98765);
+
+    // non-existent valid key
+    long getTwo = get(table, "blablah");
+    assert(getTwo == -1);
+
+    // invalid values
+    long getThree = get(table, NULL);
+    long getFour = get(table, "");
+    long getFive = get(NULL, "carrot");
+    long getSix = get(NULL, "blablah");
+    long getSeven = get(NULL, NULL);
+
+    assert(getThree + getFour + getFive + getSix + getSeven == -5);
+}
+
+void test_delete(Table * table) {    
+    // delete head in a bucket with > 1 node
+    long idxOne = hash(table, "nest");
+    bool deleteOne = delete(table, "nest");
+    assert(deleteOne);
+    assert(table->nWords == 17);
+    assert(!strcmp(table->array[idxOne]->word, "yarn"));
+
+    // delete non-head in a bucket with > 1 node
+    long idxTwo = hash(table, "under");
+    bool deleteTwo = delete(table, "under");
+    assert(deleteTwo);
+    assert(table->nWords == 16);
+    assert(!strcmp(table->array[idxTwo]->word, "boat"));
+
+    // delete only node in a bucket
+    long idxThree = hash(table, "banana");
+    bool deleteThree = delete(table, "banana");
+    assert(deleteThree);
+    assert(table->nWords == 15);
+    assert(table->array[idxThree] == NULL);
+
+    // delete a non-existent key
+    bool deleteFour = delete(table, "blablah");
+    assert(!deleteFour);
+    assert(table->nWords == 15);
+
+    // delete a NULL key
+    bool deleteFive = delete(table, NULL);
+    assert(!deleteFive);
+    assert(table->nWords == 15);
+
+    // mix of other invalid delete args
+    bool deleteSix = delete(table, "");
+    bool deleteSeven = delete(NULL, "igloo");
+    bool deleteEight = delete(NULL, "");
+    bool deleteNine = delete(NULL, "AK47");
+    
+    assert((!deleteSix) && (!deleteSeven) && (!deleteEight) && (!deleteNine));
+}
+
+void test_clear(Table * table) {
+    clear(table);
+    for (int i = 0; i < table->nBuckets; i++) {
+        assert(table->array[i] == NULL);
+    }
+
+    assert(table->nWords == 0);
+}
