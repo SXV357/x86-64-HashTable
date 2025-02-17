e98a042473383b603874961c3844803d3cb9e2ef
Shreyas Viswanathan <shrevis2018@gmail.com>
2025-02-17 16:45:41 -0600

    comments for runall.c and hash-table.h

--- src/hash-table.h
+++ src/hash-table.h
@@ -1,29 +1,27 @@
+/* Structure Definitions */
 typedef struct HashTableElement {
-    char * word; // key
-    long value; // value(can be anything)
+    char * word;
+    long value;
     struct HashTableElement * next;
 } Node;
 
 typedef struct HashTable {
-    long maxWords; // maxWords that can exist in the hash table
-    long nWords; // current number of words that exist in the hash table
-    long nBuckets; // number of buckets that exist
-    
-    // collision handling mechanism = chaining
-        // each slot in the hash table is a linked list
-    // array of pointers to HashTableElement structs
+    long maxWords;
+    long nWords;
+    long nBuckets;
     struct HashTableElement ** array;
 } Table;
 
-// function headers
-Table* init(long maxWords);
-long hash(Table * table, char * word);
+/* Function Prototypes */
 
-bool lookup(Table * table, char * word); // searches for a word and returns true/false
-bool insert(Table * table, char *word, long value); // inserts the (word, value) pair into the hash table
-bool update(Table * table, char * word, long value); // finds the word and updates its value
-bool delete(Table * table, char * word); // finds the word and deletes it
-long get(Table * table, char * word); // gets the value associated with a word
+Table * init(long);
+long hash(Table *, char *);
 
-void print(Table * table); // prints all the key value pairs in the table
-bool clear(Table * table); // clears the key value pairs in all buckets in the hash table
+bool lookup(Table *, char *);
+bool insert(Table *, char *, long);
+bool update(Table *, char *, long);
+bool delete(Table *, char *);
+long get(Table *, char *);
+
+void print(Table *);
+bool clear(Table *);
--- src/runall.c
+++ src/runall.c
@@ -1,14 +1,24 @@
+/* Shreyas Viswanathan, runall.c 
+ * Last updated Feb 17, 2025
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// utility for running all tests
+/* Constant definitions */
 
 #define RED  "\x1b[31m"
 #define RESET "\x1b[0m"
 
+/* This program is a utility to run the tests associated with both the old and 
+ * optimized C and x86-64 implementation of the hash table. There are a total of 4
+ * tests, and this program first checks whether the corresponding executable exists
+ * before running the test. If the executable doesn't exist, it throws an error and
+ * aborts the program right away.
+ */
 int main(int argc, char **argv) {
-    // Checks for c-old-test
+    // Old C implementation tests
     if (access("./c-old-test", F_OK) == 0) {
         printf(RED "Running tests for old C implementation" RESET "\n\n");
         printf("**************************************************\n");
@@ -19,7 +29,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // Checks for asm-old-test
+    // Old x86-64 Assembly tests
     if (access("./asm-old-test", F_OK) == 0) {
         printf(RED "Running tests for old x86-64 implementation" RESET "\n\n");
         printf("**************************************************\n");
@@ -30,7 +40,9 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // Checks for c-opt-test
+    /* Checks for executables associated with the optimized implementations in C and x86-64 Assembly */
+
+    // Optimized C implementation tests
     if (access("./c-opt-test", F_OK) == 0) {
         printf(RED "Running tests for new C implementation" RESET "\n\n");
         printf("**************************************************\n");
@@ -41,7 +53,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // Checks for asm-opt-test
+    // Optimized x86-64 Assembly implementation tests
     if (access("./asm-opt-test", F_OK) == 0) {
         printf(RED "Running tests for new x86-64 implementation" RESET "\n\n");
         printf("**************************************************\n");
@@ -53,4 +65,4 @@ int main(int argc, char **argv) {
     }
 
     return 0;
-}
+} /* main() */
